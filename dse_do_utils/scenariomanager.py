c99e554c78279341d78d8eda3c477f431b94a92c
vjterpstra <vjterpstra@gmail.com>
2021-11-26 20:15:51 -0500

    Fix bug

--- dse_do_utils/scenariomanager.py
+++ dse_do_utils/scenariomanager.py
@@ -855,7 +855,7 @@ class ScenarioManager(object):
         return 'PWD' in os.environ
 
     @staticmethod
-    def env_is_wscloud(self) -> bool:
+    def env_is_wscloud() -> bool:
         """Return true if environment is WS Cloud"""
         return 'PWD' in os.environ and os.environ['PWD'] == '/home/wsuser/work'
 
