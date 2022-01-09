485a8875f6f859543787e88825ec16d8e58f0764
vjterpstra <vjterpstra@gmail.com>
2022-01-09 14:04:07 -0500

    Bumping-up beta version to v0.5.4.0b

--- dse_do_utils/scenariodbmanager.py
+++ dse_do_utils/scenariodbmanager.py
@@ -22,7 +22,7 @@ from abc import ABC
 
 import sqlalchemy
 import pandas as pd
-from typing import Dict, List
+from typing import Dict, List, NamedTuple, Any
 from collections import OrderedDict
 import re
 from sqlalchemy import exc
@@ -199,6 +199,16 @@ class AutoScenarioDbTable(ScenarioDbTable):
             print("++++++++++++Integrity Error+++++++++++++")
             print(f"DataFrame insert/append of table '{table_name}'")
             print(e)
+
+
+class DbCellUpdate(NamedTuple):
+    scenario_name: str
+    table_name: str
+    row_index: List[Dict[str, Any]]  # e.g. [{'column': 'col1', 'value': 1}, {'column': 'col2', 'value': 'pear'}]
+    column_name: str
+    current_value: Any
+    previous_value: Any  # Not used for DB operation
+    row_idx: int  # Not used for DB operation
 
 
 #########################################################################
@@ -527,6 +537,8 @@ class ScenarioDbManager():
         Will set/overwrite the scenario_name in all dfs, so no need to add in advance.
         Assumes schema has been created.
         Note: there is no difference between dfs in inputs or outputs, i.e. they are inserted the same way.
+
+        TODO: break-out in a delete and an insert. Then we can re-use the insert for the duplicate API
         """
         # Step 1: delete scenario if exists
         self._delete_scenario_from_db(scenario_name, connection=connection)
@@ -551,7 +563,6 @@ class ScenarioDbManager():
         Note that it only deletes rows from tables defined in the self.db_tables, i.e. will NOT delete rows in 'auto-inserted' tables!
         Must do a 'cascading' delete to ensure not violating FK constraints. In reverse order of how they are inserted.
         Also deletes entry in scenario table
-        TODO: do within one session/cursor, so we don't have to worry about the order of the delete?
         """
         insp = sqlalchemy.inspect(self.engine)
         for scenario_table_name, db_table in reversed(self.db_tables.items()):
@@ -573,7 +584,7 @@ class ScenarioDbManager():
                                              bulk: bool = True, connection=None) -> int:
         """Specifically for single scenario replace/insert.
         Does NOT insert into the `scenario` table.
-        No `auto_insert`, i.e. only df matching db_tables.
+        No `auto_insert`, i.e. only df matching db_tables.  TODO: verify if doesn't work with AutoScenarioDbTable
         """
         num_caught_exceptions = 0
         dfs = {**inputs, **outputs}  # Combine all dfs in one dict
@@ -720,6 +731,40 @@ class ScenarioDbManager():
             df = df.drop(columns=['scenario_name'])
 
         return df
+
+    ############################################################################################
+    # Update scenario
+    ############################################################################################
+    def update_cell_changes_in_db(self, db_cell_updates: List[DbCellUpdate]):
+        """Update a set of cells in the DB.
+
+        :param db_cell_updates:
+        :return:
+        """
+        if self.enable_transactions:
+            print("Update cells with transaction")
+            with self.engine.begin() as connection:
+                self._update_cell_changes_in_db(db_cell_updates, connection=connection)
+        else:
+            self._update_cell_changes_in_db(db_cell_updates)
+
+    def _update_cell_changes_in_db(self, db_cell_updates: List[DbCellUpdate], connection=None):
+        """Update an ordered list of single value changes (cell) in the DB."""
+        for db_cell_change in db_cell_updates:
+            self._update_cell_change_in_db(db_cell_change, connection)
+
+    def _update_cell_change_in_db(self, db_cell_update: DbCellUpdate, connection=None):
+        """Update a single value (cell) change in the DB."""
+        db_table_name = self.db_tables[db_cell_update.table_name].db_table_name
+        column_change = f"{db_cell_update.column_name} = '{db_cell_update.current_value}'"
+        scenario_condition = f"scenario_name = '{db_cell_update.scenario_name}'"
+        pk_conditions = ' AND '.join([f"{pk['column']} = '{pk['value']}'" for pk in db_cell_update.row_index])
+        sql = f"UPDATE {db_table_name} SET {column_change} WHERE {pk_conditions} AND {scenario_condition};"
+        # print(f"_update_cell_change_in_db = {sql}")
+        if connection is None:
+            self.engine.execute(sql)
+        else:
+            connection.execute(sql)
 
 
     ############################################################################################
